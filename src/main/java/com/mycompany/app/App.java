e867708039a2eed1acc1a752f3372e87005a50ce
Ragalahari Akula <124599438+ragalahariakula@users.noreply.github.com>
2023-10-15 19:23:02 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -4,10 +4,21 @@ package com.mycompany.app;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "This is Jenkins:)" );
+public class Fibonacci {
+    public static void main(String[] args) {
+        int n = 10; // Change 'n' to the desired number of Fibonacci numbers.
+        
+        System.out.println("Fibonacci Series (First " + n + " numbers):");
+        for (int i = 0; i < n; i++) {
+            System.out.print(fibonacci(i) + " ");
+        }
+    }
+
+    public static int fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        } else {
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
     }
 }
